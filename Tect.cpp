b537121e482d372521f24a69a8fc7c304b3feba5
Taltosi <135122955+Taltosi@users.noreply.github.com>
2023-05-31 20:58:55 +0300

    Test.cpp

--- Tect.cpp
+++ Tect.cpp
@@ -1,36 +1,73 @@
-#include "catch.hpp"
-#include "MagicalContainer.hpp"
+#include <iostream>
+#include "doctest.h"
+#include "sources/MagicalContainer.hpp"
 
+using namespace ariel;
 
 TEST_CASE("MagicContainer"){
     MagicalContainer container;
     CHECK(container.size() == 0);
-    container.add(2);
-    container.add(8);
-    container.add(3);
+    container.addElement(2);
+    container.addElement(8);
+    container.addElement(3);
     CHECK(container.size() == 3);
 
-    container.remove(8);
+    container.removeElement(8);
     CHECK(container.size() == 2);
-    container.remove(2);
-    CHECK_THROWS(container.remove(2));
-    container.remove(3);
+    container.removeElement(2);
+    CHECK_THROWS(container.removeElement(2));
+    container.removeElement(3);
     CHECK(container.size() == 0);
 
 }
 
 TEST_CASE("AscendingIterator"){
     MagicalContainer container;
-    container.add(1);
-    container.add(2);
-    container.add(3);
-    container.add(4);
+    CHECK_NOTHROW(container.addElement(2));
+    CHECK_NOTHROW(container.addElement(5));
+    CHECK_NOTHROW(container.addElement(7));
+
+    MagicalContainer::AscendingIterator ascIter(container);
+
+    auto it = ascIter.begin();
+    CHECK_EQ(*it, 2);
+    CHECK_NOTHROW(++it);
+    CHECK_EQ(*it, 5);
+
 }
 
 TEST_CASE("SideCrossIterator"){
+    MagicalContainer container;
+    container.addElement(12);
+    container.addElement(13);
+    container.addElement(14);
+
+    MagicalContainer::SideCrossIterator sideIter(container);
+    auto it = sideIter.begin();
+    CHECK_EQ(*it, 12);
+    CHECK_NOTHROW(++it);
+    CHECK_EQ(*it, 13);
+    CHECK_NOTHROW(++it);
+    CHECK_EQ(*it, 14);
+    CHECK_NOTHROW(++it);
+
+    container.removeElement(13);   
+    CHECK(container.size() == 2);
 
 }
 
 TEST_CASE("PrimeIterator"){
+    MagicalContainer container;
+    container.addElement(12);
+    container.addElement(15);
+    container.addElement(16);
+    MagicalContainer::PrimeIterator primeIter(container);
+    MagicalContainer::PrimeIterator primeIter2(container);
 
+    auto it = primeIter.begin();
+    auto it2 = primeIter2.end();
+    CHECK_EQ(*it, 12);
+    CHECK_NOTHROW(++it);
+    CHECK_EQ(*it, 15);
+    CHECK_FALSE(primeIter != primeIter2);
 }
